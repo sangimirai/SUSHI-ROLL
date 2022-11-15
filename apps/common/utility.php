801bcd985061f582f764c5a8169862752a3272d9
Lttce <31960436+Lttce@users.noreply.github.com>
2022-11-15 07:39:38 +0000

    feat: db connection

--- apps/common/utility.php
+++ apps/common/utility.php
@@ -1,6 +1,8 @@
 <?php
 declare(strict_types=1);
 
+require_once(dirname(__FILE__)."/../../config/config.php");
+
 /**
  * htmlspecialcharsのラッパー関数
  */
@@ -8,3 +10,19 @@ function h(string $str): string {
     return htmlspecialchars($str, ENT_QUOTES);
 }
 
+/**
+ * データベースへの接続関数
+ */
+function FuncDataBaseConnection(): PDO{
+    $config = new Config();
+    $dsn = "mysql:dbname={$config->GetDbName()};host={$config->GetDbHost()};charset=utf8";
+    $user = $config->GetDbUser();
+    $password = $config->GetDbPass();
+    try {
+        $dataBase = new PDO($dsn,$user,$password);
+        return $dataBase;
+    }catch(Exception $e){
+        echo "Database Error: ". $e->getMessage();
+        exit();
+    }
+}
