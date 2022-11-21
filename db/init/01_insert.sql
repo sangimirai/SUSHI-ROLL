260f350e72f740b8f7091466a1980cda4890a1a8
Lttce <31960436+Lttce@users.noreply.github.com>
2022-11-21 10:38:34 +0000

    add: test books data

---
+++ db/init/01_insert.sql
@@ -0,0 +1,7 @@
+insert into books values
+(1, 'test title 1', 'author1', '1', '0'),
+(2, 'test title 2', 'author2', '2', '0'),
+(3, 'test title 3', 'author3', '3', '0'),
+(4, 'test title 4', 'author4', '1', '1'),
+(5, 'test title 5', 'author5', '2', '1'),
+(6, 'test title 6', 'author6', '3', '1');
