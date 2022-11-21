b487fd6dd1616b7e4ffe49fff9a42998beb00bfe
Lttce <31960436+Lttce@users.noreply.github.com>
2022-11-21 12:55:14 +0000

    add: code_types data

--- db/init/01_insert.sql
+++ db/init/01_insert.sql
@@ -5,3 +5,15 @@ insert into books values
 (4, 'test title 4', 'author4', '1', '1'),
 (5, 'test title 5', 'author5', '2', '1'),
 (6, 'test title 6', 'author6', '3', '1');
+
+insert into code_types values
+(1, 1, '歴史'),
+(1, 2, '文学'),
+(1, 3, '小説'),
+(2, 0, '利用可'),
+(2, 9, '利用不可'),
+(3, 1, '男'),
+(3, 2, '女'),
+(4, 1, '1年'),
+(4, 2, '2年'),
+(4, 3, '3年');
