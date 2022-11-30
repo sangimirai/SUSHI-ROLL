5a24947e9d89870d63c116a4ffa871bc9b8870c4
Lttce <31960436+Lttce@users.noreply.github.com>
2022-11-30 03:05:15 +0000

    add: db data

--- db/init/01_insert.sql
+++ db/init/01_insert.sql
@@ -17,3 +17,17 @@ insert into code_types values
 (4, 1, '1年'),
 (4, 2, '2年'),
 (4, 3, '3年');
+
+insert into users(user_number, user_name, sex_type, school_year, use_type, password, user_type) values
+(1, 'user1', '1', '1', '0', 'pass1', '0'),
+(2, 'user2', '2', '2', '0', 'pass2', '0'),
+(3, 'user3', '1', '3', '0', 'pass3', '0'),
+(4, 'user4', '2', '1', '0', 'pass4', '1'),
+(5, 'user5', '1', '2', '9', 'pass5', '0');
+
+insert into reception_list(reception_number, book_number, lending_user_number, lending_date, return_date) values
+(1, 1, 1, '2022-08-01', '2022-08-31'),
+(2, 1, 2, '2022-09-01', '2022-09-12'),
+(3, 1, 3, '2022-09-24', '2022-10-18'),
+(4, 2, 4, '2022-10-03', '2022-10-05'),
+(5, 2, 5, '2022-10-03', '2022-10-05');
